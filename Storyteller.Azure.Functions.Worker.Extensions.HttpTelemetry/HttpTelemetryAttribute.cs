583ebfe8e650092d2bbbf38f8f585af1095031e5
Balázs Földesi <foldesibalazs@gmail.com>
2024-09-16 10:23:09 +0200

    storyteller renaming and .net 5

---
+++ Storyteller.Azure.Functions.Worker.Extensions.HttpTelemetry/HttpTelemetryAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+[assembly: ExtensionInformation("Storyteller.Azure.WebJobs.Extensions.HttpTelemetry", "1.0.0")]
+
+namespace Storyteller.Azure.Functions.Worker.Extensions.HttpTelemetry
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public sealed class HttpTelemetryAttribute : InputBindingAttribute
+    {
+    }
+}
